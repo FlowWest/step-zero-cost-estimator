da1d6dc389b80d66b78c5af8e6f50c31a9245798
Jordan Hoang <jordan.n.hoang@gmail.com>
2022-03-25 10:42:00 -0500

    autosaves files to fix tabWidth

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,47 @@
 module.exports = {
-    siteMetadata: {
-        title: `SAFER Step Zero Cost Estimator`,
-        description: `An application to help small water systems calculate initial, high-level estimates of consolidation versus capital improvement costs`,
-        author: {
-            name: `FlowWest`,
-            content: `Solving the greatest water and natural resources challenges`
-        },
-        social: {
-            github: `https://github.com/FlowWest`
-        }
+  siteMetadata: {
+    title: `SAFER Step Zero Cost Estimator`,
+    description: `An application to help small water systems calculate initial, high-level estimates of consolidation versus capital improvement costs`,
+    author: {
+      name: `FlowWest`,
+      content: `Solving the greatest water and natural resources challenges`
     },
-    plugins: [
-        `gatsby-plugin-typescript`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: 'gatsby-plugin-material-ui',
-            // If you want to use styled components you should change the injection order.
-            options: {
-                // stylesProvider: {
-                //   injectFirst: true,
-                // },
-            }
-        },
-        {
-            resolve: `gatsby-plugin-layout`,
-            options: {
-                component: require.resolve(`./src/components/layout/index.tsx`)
-            }
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-            }
-        }
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ]
+    social: {
+      github: `https://github.com/FlowWest`
+    }
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      // If you want to use styled components you should change the injection order.
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      }
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout/index.tsx`)
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
 };
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,48 +7,48 @@ import { BsToggleOn, BsToggleOff } from 'react-icons/bs';
 import { FC } from '../../util';
 
 const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        backgroundColor: theme.palette.background.paper,
-        color: theme.palette.text.primary
-    },
-    title: {
-        flexGrow: 1
-    },
-    link: {
-        textDecoration: 'none'
-    }
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    color: theme.palette.text.primary
+  },
+  title: {
+    flexGrow: 1
+  },
+  link: {
+    textDecoration: 'none'
+  }
 }));
 
 export interface HeaderProps {
-    switchTheme: () => void;
-    siteTitle?: string;
+  switchTheme: () => void;
+  siteTitle?: string;
 }
 
 const Header: FC<HeaderProps> = (props) => {
-    const [showOn, setShowOn] = useState<boolean>(false);
-    const styles = useStyles();
+  const [showOn, setShowOn] = useState<boolean>(false);
+  const styles = useStyles();
 
-    const onSwitch = (): void => {
-        setShowOn((prev) => !prev);
-        props.switchTheme();
-    };
+  const onSwitch = (): void => {
+    setShowOn((prev) => !prev);
+    props.switchTheme();
+  };
 
-    return (
-        <AppBar component="header" position="static" className={styles.root}>
-            <Toolbar>
-                <Typography variant="h6" className={styles.title}>
-                    <Link to="/" component={GatsbyLink} color="inherit" className={styles.link}>
-                        {props.siteTitle || ''}
-                    </Link>
-                </Typography>
-                <Tooltip title="switch theme">
-                    <Button onClick={onSwitch}>
-                        {showOn ? <BsToggleOn size="40" /> : <BsToggleOff size="40" />}
-                    </Button>
-                </Tooltip>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar component="header" position="static" className={styles.root}>
+      <Toolbar>
+        <Typography variant="h6" className={styles.title}>
+          <Link to="/" component={GatsbyLink} color="inherit" className={styles.link}>
+            {props.siteTitle || ''}
+          </Link>
+        </Typography>
+        <Tooltip title="switch theme">
+          <Button onClick={onSwitch}>
+            {showOn ? <BsToggleOn size="40" /> : <BsToggleOff size="40" />}
+          </Button>
+        </Tooltip>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
