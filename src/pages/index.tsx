05d3c90b9b4880c0e8d1046f47dd9e5c797e0272
Jordan Hoang <jordan.n.hoang@gmail.com>
2022-03-25 10:37:48 -0500

    fixes tabWidth prettier setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,42 +7,42 @@ import { Seo, Hero, Lazy } from '../components';
 import { FC } from '../util';
 
 const useStyles = makeStyles((theme: Theme) => ({
-    heroButtons: {
-        marginTop: theme.spacing(4)
-    }
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  }
 }));
 
 const IndexPage: FC = () => {
-    const styles = useStyles();
-    return (
-        <>
-            <Seo title="Home" />
-            <Lazy type="grow" delay={500} timeout={1000} mountOnEnter unmountOnExit>
-                <Hero
-                    title="Hi people"
-                    description="Welcome to your new Gatsby site. Now go build something great with
+  const styles = useStyles();
+  return (
+    <>
+      <Seo title="Home" />
+      <Lazy type="grow" delay={500} timeout={1000} mountOnEnter unmountOnExit>
+        <Hero
+          title="Hi people"
+          description="Welcome to your new Gatsby site. Now go build something great with
           Typescript and Material-ui."
-                >
-                    <div className={styles.heroButtons}>
-                        <Grid container spacing={2} justify="center">
-                            <Grid item>
-                                <Lazy type="slide" direction="left" delay={500}>
-                                    <Button
-                                        component={GatsbyLink}
-                                        to="/page-two/"
-                                        variant="contained"
-                                        color="primary"
-                                    >
-                                        Go to page 2
-                                    </Button>
-                                </Lazy>
-                            </Grid>
-                        </Grid>
-                    </div>
-                </Hero>
-            </Lazy>
-        </>
-    );
+        >
+          <div className={styles.heroButtons}>
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <Lazy type="slide" direction="left" delay={500}>
+                  <Button
+                    component={GatsbyLink}
+                    to="/page-two/"
+                    variant="contained"
+                    color="primary"
+                  >
+                    Go to page 2
+                  </Button>
+                </Lazy>
+              </Grid>
+            </Grid>
+          </div>
+        </Hero>
+      </Lazy>
+    </>
+  );
 };
 
 export default IndexPage;
