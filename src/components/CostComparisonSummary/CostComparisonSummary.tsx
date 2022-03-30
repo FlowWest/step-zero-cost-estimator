08ffba35c09a38d668b58c2adeb44c936a158d72
Jordan Hoang <jordan.n.hoang@gmail.com>
2022-03-30 11:10:04 -0500

    creates cost comparison summary component and wrapper

--- src/components/CostComparisonSummary/CostComparisonSummary.tsx
+++ src/components/CostComparisonSummary/CostComparisonSummary.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import { FC } from '../../util';
+import { Theme, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import CostComparisonWrapper from './CostComparisonWrapper';
 
-const CostComparisonSummary: FC = () => {
-  return <div>CostComparisonSummary</div>;
+const useStyles = makeStyles((theme: Theme) => ({
+  totalCostLabel: {
+    fontWeight: 600
+  }
+}));
+
+const CostComparisonSummary: FC = (props) => {
+  const styles = useStyles();
+  const dummyCosts = {
+    cc: 564545289,
+    cip: 4453454335
+  };
+
+  const convertUSD = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return (
+    <div>
+      <Typography paragraph>Complete both calculators for cost comparison</Typography>
+      <Typography paragraph>
+        <span className={styles.totalCostLabel}>Consolidation Costs: </span>
+        {convertUSD.format(dummyCosts.cc)}
+      </Typography>
+      <Typography paragraph>
+        <span className={styles.totalCostLabel}>Capital Improvement Costs: </span>
+        {convertUSD.format(dummyCosts.cip)}
+      </Typography>
+      <CostComparisonWrapper />
+    </div>
+  );
 };
 
 export default CostComparisonSummary;
---
+++ src/components/CostComparisonSummary/CostComparisonWrapper.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+const CostComparisonWrapper = () => {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Consolidation" value="1" />
+            <Tab label="CIP" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Consolidation</TabPanel>
+        <TabPanel value="2">CIP</TabPanel>
+      </TabContext>
+    </Box>
+  );
+};
+
+export default CostComparisonWrapper;
