8a1adad7bee5f398460634c4ed953486a63fa75c
wdwiii <wdwhitfield3@gmail.com>
2022-04-13 15:35:07 -0400

    Remove unused interface

--- src/contexts/WaterSystem/index.tsx
+++ src/contexts/WaterSystem/index.tsx
@@ -1,12 +1,6 @@
 import React, { createContext, useReducer } from 'react';
 import { reducer, initialState } from './reducer';
 
-export interface IProps {
-  children: React.ReactNode;
-  // All other props
-  [x: string]: any;
-}
-
 export const WaterSystemContext = createContext({
   state: initialState,
   dispatch: () => null
