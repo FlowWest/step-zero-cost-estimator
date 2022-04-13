6a2309cbdf93a73b3d0239b7f2e2beeefd56c6c0
Jordan Hoang <jordan.n.hoang@gmail.com>
2022-04-13 11:31:35 -0500

    Adds cost details nested list
    Co-authored-by: wdwiii <wdwhitfield3@gmail.com>

--- src/components/CostComparisonSummary/Consolidation/ConsolidationTabs/ConsolidationDetailsTab.tsx
+++ src/components/CostComparisonSummary/Consolidation/ConsolidationTabs/ConsolidationDetailsTab.tsx
@@ -1,27 +1,73 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, List, ListItemButton, ListItemText, Collapse } from '@mui/material';
+import { ExpandMore, ExpandLess } from '@mui/icons-material';
 import { ConsolidationCostDetails } from '../../../../util/interfaces';
+import { makeStyles } from '@mui/material';
 
 const ConsolidationDetailsTab = ({ chartData }: { chartData: ConsolidationCostDetails }) => {
   const { materialCosts, adminFees, financialAssistance, adjustments, total } = chartData;
   console.log(chartData);
+
+  const [detailOpen1, setDetailOpen1] = useState(true);
+  const [detailOpen2, setDetailOpen2] = useState(true);
+  const [detailOpen3, setDetailOpen3] = useState(true);
+
+  const handleClick = (event: React.SyntheticEvent) => {
+    const eventTarget = event.target as HTMLElement;
+    let text: string = eventTarget.innerText;
+    console.log(text);
+    if (text.includes('Material Costs')) setDetailOpen1(!detailOpen1);
+    if (text.includes('Administrative Fees')) setDetailOpen2(!detailOpen2);
+    if (text.includes('Adjustments')) setDetailOpen3(!detailOpen3);
+  };
+
   return (
-    <div>
-      <Typography gutterBottom style={{ color: 'green' }}>
-        Material Costs: {materialCosts.total} <br />
-        Pipeline Costs: {materialCosts.totalPipelineCosts} <br />
-        Connection Costs: {materialCosts.totalConnectionCosts}
-      </Typography>
-      <Typography gutterBottom style={{ color: 'purple' }}>
-        Administrative Fees: {adminFees.total} <br />
-        Admin and Legal: {adminFees.adminLegalCosts}
-      </Typography>
-      <Typography gutterBottom style={{ color: 'orange' }}>
-        Adjustments: {adjustments.total} <br />
-        Contingency: {adjustments.totalContingency}
-      </Typography>
-      <Typography gutterBottom>TOTAL MERGER COST: {total}</Typography>
-    </div>
+    <>
+      <List
+        sx={{ width: '100%', maxWidth: 600, bgcolor: '#fff' }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        {/* First List Item */}
+        <ListItemButton divider onClick={handleClick}>
+          <ListItemText primary={`Material Costs: ${materialCosts.total}`} />
+          {detailOpen1 ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={detailOpen1} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 5 }}>
+              <ListItemText primary={'Testing A1'} />
+            </ListItemButton>
+          </List>
+        </Collapse>
+
+        {/* Second List Item */}
+        <ListItemButton divider onClick={handleClick}>
+          <ListItemText primary={`Administrative Fees: ${adminFees.total}`} />
+          {detailOpen2 ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={detailOpen2} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 5 }}>
+              <ListItemText primary={'Testing B2'} />
+            </ListItemButton>
+          </List>
+        </Collapse>
+
+        {/* Third List Item */}
+        <ListItemButton divider onClick={handleClick}>
+          <ListItemText primary={`Adjustments: ${adjustments.total}`} />
+          {detailOpen3 ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={detailOpen3} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 5 }}>
+              <ListItemText primary={'Testing C3'} />
+            </ListItemButton>
+          </List>
+        </Collapse>
+      </List>
+    </>
   );
 };
 
--- src/components/CostComparisonSummary/Consolidation/TotalConsolidationCost.tsx
+++ src/components/CostComparisonSummary/Consolidation/TotalConsolidationCost.tsx
@@ -1,14 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Typography, Theme } from '@mui/material';
+import { Typography, Theme, Grid } from '@mui/material';
 import { WaterSystemContext } from '../../../contexts/WaterSystem';
 import { getConsolidationCostDetails } from '../../../util/consolidationUtil';
 import { makeStyles } from '@mui/styles';
 import ConsolidationTabs from './ConsolidationTabs/ConsolidationTabs';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  totalCostContainer: {
+  componentsContainer: {
+    height: '100%',
     textAlign: 'center',
     paddingLeft: '1rem'
+  },
+  gridItem: {
+    width: '100%'
   }
 }));
 
@@ -33,35 +37,44 @@ const TotalConsolidationCost = () => {
   }, [state.consolidationCostParams]);
 
   return (
-    <div className={styles.totalCostContainer}>
-      <Typography variant="h5" paragraph>
-        Total Consolidation Cost: $
-        {consolidationCostDetails.total?.toLocaleString('en-US', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2
-        })}
-      </Typography>
-      <Typography variant="h6" paragraph>
-        Cost per Connection: $
-        {(
-          consolidationCostDetails.total /
-          consolidationCostParams.connections /
-          12 /
-          10
-        ).toLocaleString('en-US', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2
-        })}
-      </Typography>
-      {/* {Object.entries(state.consolidationCostParams)?.map(([key, value]: any) => {
-        return (
-          <div>
-            {key}: {value.toLocaleString()}
-          </div>
-        );
-      })} */}
-      <ConsolidationTabs chartData={consolidationCostDetails} />
-    </div>
+    <Grid
+      className={styles.componentsContainer}
+      container
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Grid item className={styles.gridItem}>
+        <Typography variant="h5" paragraph>
+          Total Consolidation Cost: $
+          {consolidationCostDetails.total?.toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+          })}
+        </Typography>
+        <Typography variant="h6" paragraph>
+          Cost per Connection: $
+          {(
+            consolidationCostDetails.total /
+            consolidationCostParams.connections /
+            12 /
+            10
+          ).toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+          })}
+        </Typography>
+        <ConsolidationTabs chartData={consolidationCostDetails} />
+      </Grid>
+      <Grid item className={styles.gridItem}>
+        <Typography paragraph sx={{ fontStyle: 'italic' }}>
+          The information provided on this page is for a step zero analysis and is intended for
+          exploratory purposes only. The calculations are not a replacement for a full feasibility
+          analysis.{' '}
+        </Typography>
+      </Grid>
+    </Grid>
+    // </div>
   );
 };
 
