cd603cf1d3e431f5113b207f601fc9c43e364e04
wdwiii <wdwhitfield3@gmail.com>
2022-04-28 14:05:19 -0400

    Render components for capital improvement calcultr

--- src/components/CostComparisonSummary/CapitalImprovement/CapitalImprovementCalculator.tsx
+++ src/components/CostComparisonSummary/CapitalImprovement/CapitalImprovementCalculator.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Typography, Grid, Theme, TextField, Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import WaterSystemDetailsCIP from './WaterSystemDetailsCIP';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  cipHeader: {
+    fontWeight: 600
+  }
+}));
 
 function CapitalImprovementCalculator(props: any) {
-  return <div>CapitalImprovementCalculator</div>;
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      rowSpacing={3}
+      // style={{
+      //   paddingBottom: '1rem',
+      //   borderBottom: `3px solid ${theme.palette.divider}`
+      // }}
+    >
+      <Grid item xs={12}>
+        <Typography variant="h6" className={classes.cipHeader}>
+          Capital Improvement Calculator
+        </Typography>
+      </Grid>
+      <WaterSystemDetailsCIP />
+      <Grid item>
+        <Button variant="contained">Add Components</Button>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default CapitalImprovementCalculator;
---
+++ src/components/CostComparisonSummary/CapitalImprovement/ComponentsGrid.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ComponentsGrid = () => {
+  return <div>ComponentsGrid</div>;
+};
+
+export default ComponentsGrid;
