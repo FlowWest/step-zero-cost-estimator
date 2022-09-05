b47c7f9d41d1ae4d42c14dc04ad1a89fa1b2a44b
wdwiii <wdwhitfield3@gmail.com>
2022-09-05 13:25:26 -0400

    Add second page with tables and layout options

--- src/components/pdf/PdfRender.tsx
+++ src/components/pdf/PdfRender.tsx
@@ -5,13 +5,13 @@ import Header from './Header';
 import WaterSystemDetailsPdf from './WaterSystemDetailsPdf';
 import ComponentsTable from './ComponentsTable';
 import FeesTable from './FeesTable';
+import { getConsolidationCostDetails } from '../../util/costUtil';
 // Create styles
-
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'column',
+    flexDirection: 'column', //original styles
     backgroundColor: '#fff',
-    padding: 25
+    padding: '15px 25px'
   },
   section: {
     margin: 10,
@@ -19,18 +19,31 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     display: 'flex'
   },
+  chartWrapper: {
+    //width: '45%' //landscape
+    width: '75%', //alt landscape
+    transform: 'translateX(15px)'
+  },
   chartImage: {
-    width: '45%',
-    marginTop: 35,
-    marginRight: 10
+    marginTop: 15
+    //width: '45%',//portrait
+    //marginRight: 10 //portrait
+    //height: '100px' //landscape
+    //position: 'absolute'
+    //transform: 'translate(45px 35px) scale(2)' //landscape
+    //height: '300px' //alt landscape
+    //width: '100%' //alt landscape
   },
   grid: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'flex-start',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    width: '100%'
   },
-  disclaimerText: { color: 'rgba(0,0,0,.4)', fontSize: 9, marginTop: 25, fontStyle: 'italic' }
+  gridInverted: { display: 'flex', flexDirection: 'column' },
+  disclaimerText: { color: 'rgba(0,0,0,.4)', fontSize: 9, marginTop: 15, fontStyle: 'italic' },
+  totalConsolidationCost: { fontSize: 12 }
 });
 
 // Create Document Component
@@ -42,19 +55,27 @@ const PdfRender = ({ state }: { state: any }): JSX.Element => {
       : consolidationChart?.toBase64Image();
 
   console.log('🚀 ~ PdfRender ~ imageData', imageData);
+  const { total: totalConsolidationCost } = getConsolidationCostDetails(state);
 
   return (
     <Document>
-      <Page size="A4" style={styles.page}>
+      <Page size="A4" orientation="landscape" style={styles.page}>
         <Header title="Consolidation Report" />
-        <View style={styles.grid}>
-          <WaterSystemDetailsPdf state={state} />
-          <Image src={imageData} style={styles.chartImage} />
+        {/* Data Wrapper - For alt layout */}
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          {/* <View style={styles.grid}> Original Styles*/}
+          <View style={{ width: '50%', display: 'flex', flexDirection: 'column-reverse' }}>
+            <WaterSystemDetailsPdf state={state} />
+            <View style={styles.chartWrapper}>
+              <Image src={imageData} style={styles.chartImage} />
+            </View>
+          </View>
+          {/* <View style={styles.grid}> Original Styles*/}
+          <View style={{ width: '50%' }}>
+            <FeesTable state={state} />
+          </View>
         </View>
-        <View style={styles.grid}>
-          <FeesTable state={state} />
-        </View>
-        <View style={styles.grid}>
+        <View style={[styles.grid, { justifyContent: 'center' }]}>
           <Text style={styles.disclaimerText}>
             The information provided on this page is for a step zero analysis and is intended for
             exploratory purposes only. The calculations are not a replacement for a full feasibility
@@ -62,13 +83,17 @@ const PdfRender = ({ state }: { state: any }): JSX.Element => {
           </Text>
         </View>
       </Page>
-      <Page size="A4" style={styles.page}>
+      <Page size="A4" orientation="landscape" style={styles.page}>
         <Header title="Capital Improvement Report" />
-        <View style={styles.grid}>
-          <WaterSystemDetailsPdf state={state} />
-        </View>
-        <View style={styles.grid}>
-          <ComponentsTable state={state} age="new" />
+        <View style={styles.gridInverted}>
+          <ComponentsTable state={state} type="total" />
+          <ComponentsTable state={state} type="existing" />
+          <ComponentsTable state={state} type="new" />
+          <Text style={[styles.disclaimerText, { alignSelf: 'center' }]}>
+            The information provided on this page is for a step zero analysis and is intended for
+            exploratory purposes only. The calculations are not a replacement for a full feasibility
+            analysis.
+          </Text>
         </View>
       </Page>
     </Document>
