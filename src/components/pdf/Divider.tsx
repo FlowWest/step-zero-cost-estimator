0944793112c71efe688bfcda12f040614a9b7e2f
wdwiii <wdwhitfield3@gmail.com>
2022-09-05 13:23:47 -0400

    Modify styles for divider

--- src/components/pdf/Divider.tsx
+++ src/components/pdf/Divider.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
-  divider: { height: '1px', width: '100%', backgroundColor: 'rgba(29,73,118)', margin: '0 auto' }
+  divider: { height: '1px', width: '100%', backgroundColor: 'rgba(29,73,118)', marginTop: '10px' }
 });
 
 const Divider = () => {
