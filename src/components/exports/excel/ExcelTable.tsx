4ba39da74791e21579a9c963fa856c65ae76b461
wdwiii <wdwhitfield3@gmail.com>
2022-09-15 14:28:36 -0400

    Refactor Excel Export Function
    - Create JSX Table
    - Capture Table Image into string
    - Render excel from htmlstring

---
+++ src/components/exports/excel/ExcelTable.tsx
@@ -0,0 +1,186 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { TransformedSystemComponent } from '../../../util/interfaces';
+import { formatToUSD } from '../../../util';
+import { startCase } from 'lodash';
+
+export const ExcelTable = (state: any) => {
+  const connections = state?.consolidationCostParams?.connections;
+
+  function renderEmptyData(num: number, type: 'row' | 'cell') {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+      if (type === 'row') arr.push(<tr></tr>);
+      if (type === 'cell') arr.push(<td></td>);
+    }
+    return arr;
+  }
+
+  function getDataRows(componentAge: 'new' | 'existing') {
+    return state[`${componentAge}Components`]?.map((component: TransformedSystemComponent) => {
+      const { annualReserve, avgLife, component: name, monthlyReserve, unitCost } = component;
+
+      return (
+        <tr key={component.uid}>
+          <td>1</td>
+          <td colSpan={5}>{name}</td>
+          <td>{formatToUSD(unitCost)}</td>
+          <td>{formatToUSD(unitCost)}</td>
+          <td>{avgLife}</td>
+          <td>{formatToUSD(annualReserve)}</td>
+          <td>{formatToUSD(monthlyReserve)}</td>
+          <td>{formatToUSD(monthlyReserve / connections)}</td>
+        </tr>
+      );
+    });
+  }
+
+  function generateSubtotals(componentAge: 'new' | 'existing' | 'all') {
+    console.log('state', state);
+    const components =
+      componentAge === 'all'
+        ? [...state?.existingComponents, ...state?.newComponents]
+        : [...state[`${componentAge}Components`]];
+    console.log('🚀 ~ generateSubtotals ~ components', components);
+    const totals = components.reduce(
+      (total, currentValue) => {
+        return {
+          installedCost: (total.installedCost += currentValue.unitCost),
+          annualReserve: (total.annualReserve += currentValue.annualReserve),
+          monthlyReserve: (total.monthlyReserve += currentValue.monthlyReserve),
+          monthlyReservePerCustomer: (total.monthlyReservePerCustomer +=
+            currentValue.monthlyReserve / connections)
+        };
+      },
+      {
+        installedCost: 0,
+        annualReserve: 0,
+        monthlyReserve: 0,
+        monthlyReservePerCustomer: 0
+      }
+    );
+    console.log('🚀 ~ totals ~ totals', totals);
+
+    return (
+      <tr>
+        <td></td>
+        <td colSpan={5}>
+          {componentAge === 'all'
+            ? 'TOTAL Existing and New Project CIP'
+            : `SUBTOTAL ${startCase(componentAge)} CIP Costs`}
+        </td>
+        <td></td>
+        <td>{formatToUSD(totals.installedCost)}</td>
+        <td></td>
+        <td>{formatToUSD(totals.annualReserve)}</td>
+        <td>{formatToUSD(totals.monthlyReserve)}</td>
+        <td>{formatToUSD(totals.monthlyReservePerCustomer)}</td>
+      </tr>
+    );
+  }
+
+  function renderSignatureAndNotes() {
+    return (
+      <>
+        <tr>
+          <td></td>
+          <td colSpan={7}>
+            Report Prepared by (Title):
+            _________________________________________________________________
+          </td>
+          {renderEmptyData(1, 'cell')}
+          <td colSpan={3}>Date: ________________________________</td>
+        </tr>
+        <tr>
+          {renderEmptyData(2, 'cell')}
+          <td colSpan={7}>
+            NOTE: Installed costs are averages and include all materials and contracted labor and
+            equipment.
+          </td>
+        </tr>
+        {renderEmptyData(1, 'row')}
+        <tr>
+          <td colSpan={13} rowSpan={5}>
+            NOTES:
+          </td>
+        </tr>
+      </>
+    );
+  }
+
+  return (
+    <table
+      id="test-table"
+      className="table table-striped"
+      style={{ width: '100%', textAlign: 'center' }}
+    >
+      <thead>
+        <tr>
+          <td></td>
+          <th colSpan={4}>CAPITAL IMPROVEMENT PLAN (CIP)</th>
+        </tr>
+        <tr>
+          {renderEmptyData(7, 'cell')}
+          <td colSpan={2}>Date:</td>
+          <td>{new Date().toLocaleString()}</td>
+        </tr>
+        <tr>
+          {renderEmptyData(7, 'cell')}
+          <td colSpan={2}>System ID No.:</td>
+          <td>{state?.currentWaterSystem?.joinSystemPWSID}</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td colSpan={2}>System Name:</td>
+          <td colSpan={4}>{state?.currentWaterSystem?.joinSystemName}</td>
+          <td colSpan={2}>Service Connections:</td>
+          <td>{connections}</td>
+        </tr>
+        <tr>
+          {renderEmptyData(11, 'cell')}
+          <td>MONTHLY</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td colSpan={5}>*Enter information only in YELLOW shaded cells</td>
+          {renderEmptyData(2, 'cell')}
+          <td>AVG</td>
+          {renderEmptyData(2, 'cell')}
+          <td>RESERVE</td>
+        </tr>
+        <tr>
+          {renderEmptyData(6, 'cell')}
+          <td>UNIT</td>
+          <td>INSTALLED</td>
+          <td>LIFE,</td>
+          <td>ANNUAL</td>
+          <td>MONTHLY</td>
+          <td>PER</td>
+        </tr>
+        <tr>
+          <td>QTY</td>
+          <td colSpan={5}>COMPONENT</td>
+          <td>COST</td>
+          <td>COST</td>
+          <td>YEARS</td>
+          <td>RESERVE</td>
+          <td>RESERVE</td>
+          <td>CUSTOMER</td>
+        </tr>
+      </thead>
+      <tbody>
+        {getDataRows('existing')}
+        {generateSubtotals('existing')}
+        {renderEmptyData(2, 'row')}
+        {getDataRows('new')}
+        {generateSubtotals('new')}
+        {renderEmptyData(1, 'row')}
+        {generateSubtotals('all')}
+        {renderEmptyData(3, 'row')}
+        {renderSignatureAndNotes()}
+      </tbody>
+    </table>
+  );
+};
+
+export const generateHtmlString = (element: any) => ReactDOMServer.renderToStaticMarkup(element);
--- src/components/exports/excel/excelUtility.ts
+++ src/components/exports/excel/excelUtility.ts
@@ -1,20 +1,98 @@
-import { read, utils, writeFile } from 'xlsx';
+import { utils, writeFile, read } from 'xlsx-js-style';
 import axios from 'axios';
+import { ExcelTable, generateHtmlString } from './ExcelTable';
 
-export const handleExcelExport = async (state: any, templateFileNode: Record<string, unknown>) => {
+export const handleExcelExport = async (state: any, templateFileNode: any) => {
+  const htmlString = generateHtmlString(ExcelTable(state));
+  console.log('🚀 ~ handleExcelExportV2 ~ htmlString', htmlString);
+  console.log('🚀 ~ handleExcelExportV2 ~ state', state);
+
   const url = templateFileNode.publicURL as string;
 
+  const test = await axios({
+    method: 'GET',
+    url,
+    responseType: 'arraybuffer'
+  });
+  const data = new Uint8Array(test.data);
+  const workbookTest = read(data, { type: 'array', cellStyles: true, cellNF: true }) as any;
+
+  console.log('wbt', workbookTest);
+
   try {
-    const test = await axios({
-      method: 'GET',
-      url,
-      responseType: 'arraybuffer'
-    });
-    const data = new Uint8Array(test.data);
-    const workbook = read(data, { cellStyles: true });
+    const elt = document.createElement('div');
+    elt.innerHTML = htmlString;
+    document.body.appendChild(elt);
 
+    /* generate worksheet */
+    const workbook = utils.table_to_book(elt, { sheet: 'CIP' });
+
+    /* remove element */
+    document.body.removeChild(elt);
+
+    const cipSheet = workbook.Sheets.CIP as Record<string, any>;
+
+    // cipSheet['!merges'] = [
+    //   {
+    //     s: { c: 1, r: 3 },
+    //     e: { c: 2, r: 3 }
+    //   },
+    //   {
+    //     s: { c: 3, r: 3 },
+    //     e: { c: 6, r: 3 }
+    //   }
+    // ];
+
+    cipSheet.B1.s = {
+      ...cipSheet.B1.s,
+      font: {
+        color: { rgb: '000000' },
+        family: 2,
+        name: 'Arial',
+        sz: 14
+      }
+    };
     console.log('wb', workbook);
+    addSystemNameStyling(cipSheet);
+    console.log('wb', workbook);
+    writeFile(workbook, `swsbudgetcalculator_${new Date().toLocaleDateString()}.xlsx`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    writeFile(workbook, 'test_excel.xlsx');
-  } catch (error) {}
+const addHighlight = () => {
+  return {
+    bgColor: { rgb: 'FFFF99' },
+    fgColor: { rgb: 'FFFF00' },
+    patternType: 'solid'
+  };
+};
+
+const addBorder = () => {
+  return {
+    top: { style: 'thin' },
+    bottom: { style: 'thin' },
+    left: { style: 'thin' },
+    right: { style: 'thin' }
+  };
+};
+
+const addSystemNameStyling = (cipSheet: any) => {
+  const cells = ['D4', 'E4', 'F4', 'G4'];
+  cells.forEach((cell) => {
+    if (cipSheet[cell]) {
+      cipSheet[cell].s = {
+        fill: addHighlight(),
+        border: addBorder()
+      };
+    } else {
+      cipSheet[cell] = {
+        s: {
+          fill: addHighlight(),
+          border: addBorder()
+        }
+      };
+    }
+  });
 };
