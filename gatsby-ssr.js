9686ecd7e96f204475a88d01087043dbf0d74c38
Jordan Hoang <jordan.n.hoang@gmail.com>
2022-11-29 17:15:53 -0600

    fix build error

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,3 +3,26 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider, StyledEngineProvider, createTheme } from '@mui/material';
+import { WaterSystemProvider } from './src/contexts/WaterSystem';
+import { FCR, themes, getCookie, setCookie, themeCookie } from './src/util';
+import makeStyles from '@mui/styles/makeStyles';
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes['light']}>
+        <WaterSystemProvider>{element}</WaterSystemProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
+
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback);
+  };
+}
